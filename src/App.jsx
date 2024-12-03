98aec5d9f9ca4abaf1e135b31ea0a70b911e7780
Alejandro PC <alejandromartin.ro.ce@gmail.com>
2024-12-03 21:50:49 +0100

    Inicio de Header

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,6 @@ import Header from "./components/Header/Header";
 // import Footer from "./components/Footer/Footer";
 import "./App.css";
 
-
-
 function App() {
   return (
     <>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import "./Header.css";
+import { useState } from 'react';
+import './Header.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para controlar la apertura del menú
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Cambiar el estado del menú
+  };
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+    <header className="header">
+      <nav className="header__nav">
+        <h1 className="header__nombre">Alejandro Martín</h1>
+        <div className="header__hamburguesa" onClick={toggleMenu}>
+          <div className="iconohamburguesa"></div>
+        </div>
+        <ul className={`header__menu ${menuOpen ? 'header__menu--open' : ''}`}>
+          <li className="header__item"><a href="#home" className="header__link">Home</a></li>
+          <li className="header__item"><a href="#about" className="header__link">About</a></li>
+          <li className="header__item"><a href="#projects" className="header__link">Projects</a></li>
+          <li className="header__item"><a href="#contact" className="header__link">Contact</a></li>
         </ul>
       </nav>
     </header>
